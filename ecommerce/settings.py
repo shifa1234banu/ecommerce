e6bec236092e6bab707867ef8137de26001714ca
shifa1234banu <shifabanum212@gmail.com>
2021-07-16 11:46:47 +0530

    hello

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shifa',
+        'USER':'shifauser',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
